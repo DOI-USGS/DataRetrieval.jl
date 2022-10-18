50b2775347791acc6a23537f39e99f565fce21ec
elbeejay <jhariharan@usgs.gov>
2022-10-18 13:21:40 -0400

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/DataRetrieval.jl
@@ -0,0 +1,5 @@
+module DataRetrieval
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DataRetrieval
+using Test
+
+@testset "DataRetrieval.jl" begin
+    # Write your tests here.
+end
